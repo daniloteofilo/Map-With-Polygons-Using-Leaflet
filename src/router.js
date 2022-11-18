0f030d6340575e69a2977cb61a7c7d11e6d09b5a
Danilo Teofilo <danigolaseu@gmail.com>
2022-11-18 15:39:07 -0300

    Versão clean

--- src/router.js
+++ src/router.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import MapLeafLet from './components/MapLeafLet'
 import HomePage from "./pages/HomePage";
-// import LoginPage from './pages/LoginPage'
-import RegisterPage from "./pages/RegisterPage";
 
 function Router() {
-	return (
-		<Routes>
-            <Route path='/' element={<HomePage />} />
-			<Route path="/register" element={<RegisterPage/>} />
-			<Route path="/mapa" element={<MapLeafLet/>} />
-			<Route path="/home" element={<HomePage/>} />
-        </Routes>
-	);
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
+  );
 }
 export default Router;
